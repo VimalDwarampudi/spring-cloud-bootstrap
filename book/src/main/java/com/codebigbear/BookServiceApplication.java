17ae1e8bff18cc92fc67134f3389d2c3b72be814
Vimal Dwarampudi <vimalananda4@gmail.com>
2018-09-12 21:46:04 -0400

    working config,eureka,gateway,rest api,zipkin and sleuth

--- book/src/main/java/com/codebigbear/BookServiceApplication.java
+++ book/src/main/java/com/codebigbear/BookServiceApplication.java
@@ -1,6 +1,5 @@
 package com.codebigbear;
 
-import com.codebigbear.book.Book;
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,18 +10,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.sleuth.metric.SpanMetricReporter;
 import org.springframework.cloud.sleuth.zipkin.HttpZipkinSpanReporter;
 import org.springframework.cloud.sleuth.zipkin.ZipkinProperties;
-import org.springframework.cloud.sleuth.zipkin.ZipkinRestTemplateCustomizer;
 import org.springframework.cloud.sleuth.zipkin.ZipkinSpanReporter;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import zipkin.Span;
-
-import java.util.Arrays;
-import java.util.List;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -41,33 +31,19 @@ public class BookServiceApplication {
         SpringApplication.run(BookServiceApplication.class, args);
     }
 
-
     @Bean
     public ZipkinSpanReporter makeZipkinSpanReporter() {
         return new ZipkinSpanReporter() {
             private HttpZipkinSpanReporter delegate;
             private String baseUrl;
-            private ZipkinRestTemplateCustomizer zipkinRestTemplateCustomizer;
 
             @Override
             public void report(Span span) {
-
-                InstanceInfo instance = eurekaClient
-                        .getNextServerFromEureka("zipkin", false);
-                if (!(baseUrl != null &&
-                        instance.getHomePageUrl().equals(baseUrl))) {
+                InstanceInfo instance = eurekaClient.getNextServerFromEureka("zipkin", false);
+                if (!(baseUrl != null && instance.getHomePageUrl().equals(baseUrl))) {
                     baseUrl = instance.getHomePageUrl();
-                    RestTemplate restTemplate = new RestTemplate();
-                    zipkinRestTemplateCustomizer.customize(restTemplate);
-                    delegate = new HttpZipkinSpanReporter(
-                            restTemplate,
-                            baseUrl,
-                            zipkinProperties.getFlushInterval(),
-                            spanMetricReporter);
-                }
-
-                if (!span.name.matches(skipPattern)) {
-                    delegate.report(span);
+                    delegate = new HttpZipkinSpanReporter(baseUrl, zipkinProperties.getFlushInterval(), zipkinProperties.getCompression().isEnabled(), spanMetricReporter);
+                    if (!span.name.matches(skipPattern)) delegate.report(span);
                 }
             }
         };
